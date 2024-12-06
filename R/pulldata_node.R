6161e7a92565aaa7fcdfe8a986949f13dddb134c
Kate Esbenshade <ksesbenshade@gmail.com>
2024-12-06 15:18:13 -0800

    started two functions to allow users to pull different types of data. time specification is not working

---
+++ R/pulldata_node.R
@@ -0,0 +1,5 @@
+#' Pull Data from One Node
+#' 
+#' This function takes in a city or a node and a year and returns a dataframe of the price every 15 minutes for that year in that location
+#' 
+#' @param node city (automatically set to NULL)
