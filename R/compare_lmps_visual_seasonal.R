54d929f3a60e726a2d34bc99ccf938c5e78833cf
Kate Esbenshade <ksesbenshade@gmail.com>
2024-12-07 22:56:33 -0800

    seasonal lmp comparison

--- R/compare_lmps_visual_seasonal.R
+++ R/compare_lmps_visual_seasonal.R
@@ -1,23 +1,31 @@
-#' Compare LMPs between two cities visually
+#' Compare LMPs between two cities visually by Season
 #' 
-#' This script reads in two datasets containing LMPs for two different cities over a year (or other specified time period) and compares the distribution of LMPs by hour of the day.
+#' This script reads in two datasets containing LMPs for two different cities over a year (or other specified time period) and compares the distribution of LMPs by hour of the day by season
 #' 
 #' @param df1 A tibble containing LMP data for the first city over a year
 #' @param df2 A tibble containing LMP data for the second city over a year
 #' 
-#' @return A ggplot object displaying the distribution of LMPs by hour of the day for the two cities
+#' @return A ggplot object displaying the distribution of LMPs by hour of the day for the two cities by season
 #' 
 #' @import dplyr
 #' @import ggplot2
 #' 
 #' @export
-compare_lmps_visual = function(df1, df2){
+compare_lmps_visual_seasonal = function(df1, df2){
   df1 <- df1 %>%
     mutate(
       interval_start_utc = as.POSIXct(interval_start_utc, format = "%Y-%m-%d %H:%M:%S", tz = "UTC"),
       interval_start_pst = lubridate::with_tz(interval_start_utc, tzone = "America/Los_Angeles"),
       time_only = format(interval_start_pst, "%H:%M:%S"),
-      hour = as.numeric(substr(time_only, 1, 2))
+      hour = as.numeric(substr(time_only, 1, 2)),
+      month = as.numeric(format(interval_start_pst, "%m")),
+      season = case_when(
+        month %in% c(12, 1, 2) ~ "Winter",
+        month %in% c(3, 4, 5) ~ "Spring",
+        month %in% c(6, 7, 8) ~ "Summer",
+        month %in% c(9, 10, 11) ~ "Fall",
+        TRUE ~ NA_character_
+      )
     )
   
   df2 <- df2 %>%
@@ -25,12 +33,20 @@ compare_lmps_visual = function(df1, df2){
       interval_start_utc = as.POSIXct(interval_start_utc, format = "%Y-%m-%d %H:%M:%S", tz = "UTC"),
       interval_start_pst = lubridate::with_tz(interval_start_utc, tzone = "America/Los_Angeles"),
       time_only = format(interval_start_pst, "%H:%M:%S"),
-      hour = as.numeric(substr(time_only, 1, 2))
+      hour = as.numeric(substr(time_only, 1, 2)),
+      month = as.numeric(format(interval_start_pst, "%m")),
+      season = case_when(
+        month %in% c(12, 1, 2) ~ "Winter",
+        month %in% c(3, 4, 5) ~ "Spring",
+        month %in% c(6, 7, 8) ~ "Summer",
+        month %in% c(9, 10, 11) ~ "Fall",
+        TRUE ~ NA_character_
+      )
     )
   
   avg_price_per_time1 <- df1 %>%
     filter(time_only == paste0(sprintf("%02d", hour), ":00:00")) %>% 
-    group_by(hour) %>%
+    group_by(hour, season) %>%
     summarise(
       lower_whisker = quantile(lmp, 0.1, na.rm = TRUE),
       lower_quartile = quantile(lmp, 0.25, na.rm = TRUE),
@@ -42,7 +58,7 @@ compare_lmps_visual = function(df1, df2){
   
   avg_price_per_time2 <- df2 %>%
     filter(time_only == paste0(sprintf("%02d", hour), ":00:00")) %>% 
-    group_by(hour) %>%
+    group_by(hour, season) %>%
     summarise(
       lower_whisker = quantile(lmp, 0.1, na.rm = TRUE),
       lower_quartile = quantile(lmp, 0.25, na.rm = TRUE),
@@ -155,6 +171,7 @@ compare_lmps_visual = function(df1, df2){
       subtitle = "Median, Interquartile Range, and 10% and 90% whiskers",
       y = "Locational Marginal Price ($/MWh)"
     ) +
+    facet_wrap(~season, scales = "fixed", ncol = 2) + # Facet by season
     theme_solarized() +
     theme(
       axis.text.x = element_text(angle = 90, vjust = 0.5)
